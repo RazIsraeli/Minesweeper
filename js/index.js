19258cbcab26909cec02f05ca9b509a0aa97fba1
Raz Israeli <109732328+RazIsraeli@users.noreply.github.com>
2022-09-22 02:07:06 +0300

    Added support for 3 levels + half baked recursion opening
    The game now supports 3 levels:
    beginner, advanced and expert.
    The game partially supports recursion opening (only from the top-right to the bottom-left side)

--- js/index.js
+++ js/index.js
@@ -9,12 +9,11 @@ var gBoard //start with 4X4 board
 var gMines //start with 2
 var gTimeInterval
 
-var gLevel = {
-  size: 8,
-  mines: 14,
-}
+var gLevel
 
-function onInit() {
+function onInit(level = { size: 4, mines: 2 }) {
+  gLevel = level
+
   gGame = {
     isOn: false,
     shownCount: 0,
@@ -22,6 +21,7 @@ function onInit() {
     secsPassed: 0,
     isWin: false,
   }
+
   gMines = []
   document.addEventListener('contextmenu', (event) => event.preventDefault())
   gBoard = buildBoard(gLevel.size)
@@ -54,10 +54,12 @@ function onCellClicked(elCell, cellI, cellJ) {
     elCell.classList.add('shown')
     elCell.style.backgroundColor = 'rgba(255,0,0,0.4)'
     gameOver(gGame.isWin)
+    return
   } else if (gBoard[cellI][cellJ].mineNegsCount) {
     // update model
     gBoard[cellI][cellJ].isShown = true
     gGame.shownCount++
+    console.log(gGame.shownCount)
     // update DOM
     const value = gBoard[cellI][cellJ].mineNegsCount
     elCell.innerText = value
@@ -65,18 +67,21 @@ function onCellClicked(elCell, cellI, cellJ) {
     renderCell({ i: cellI, j: cellJ }, value)
     if (Math.pow(gLevel.size, 2) === gGame.shownCount + gGame.markedCount)
       checkGameOver()
-  } else if (
-    !gBoard[cellI][cellJ].mineNegsCount &&
-    !gBoard[cellI][cellJ].isMine
-  ) {
-    showNegs(gBoard, cellI, cellJ)
-    var elCell = document.querySelector(`.cell-${cellI}-${cellJ}`)
-    gBoard[cellI][cellJ].isShown = true
-    gGame.shownCount++
-    elCell.classList.add('shown')
-    if (Math.pow(gLevel.size, 2) === gGame.shownCount + gGame.markedCount)
-      checkGameOver()
-  }
+  } else showNegs1(gBoard, cellI, cellJ)
+  // else if (
+  //   !gBoard[cellI][cellJ].mineNegsCount &&
+  //   !gBoard[cellI][cellJ].isMine
+  // ) {
+  // showNegs(gBoard, cellI, cellJ)
+  // update model
+  // gBoard[cellI][cellJ].isShown = true
+  // gGame.shownCount++
+  // console.log(gGame.shownCount)
+  // update DOM
+  // var elCell = document.querySelector(`.cell-${cellI}-${cellJ}`)
+  // elCell.classList.add('shown')
+  if (Math.pow(gLevel.size, 2) === gGame.shownCount + gGame.markedCount)
+    checkGameOver()
 }
 
 function checkGameOver() {
@@ -113,6 +118,7 @@ function cellMarked(elCell, cellI, cellJ) {
     // update model
     gBoard[cellI][cellJ].isMarked = true
     gGame.markedCount++
+    console.log(gGame.markedCount)
     //update DOM
     elCell.innerHTML = FLAG
     if (Math.pow(gLevel.size, 2) === gGame.shownCount + gGame.markedCount)
@@ -211,13 +217,13 @@ function buildBoard(size) {
   return board
 }
 
-function restart() {
+function restart(level = { size: 4, mines: 2 }) {
   //clear all settings and reset timer
   gameOver(gGame.isWin)
   document.querySelector('.time span').innerText = gGame.secsPassed
   document.querySelector('.game-icon img').src = 'images/smiley-regular.png'
   gTimeInterval = 0
-  onInit()
+  onInit(level)
 }
 
 function setMinesNegsCount(board, rowIdx, colIdx) {
@@ -244,7 +250,6 @@ function createMines() {
     var randIdx = getRandomInt(0, emptyLocations.length)
 
     var randLocation = emptyLocations.splice(randIdx, 1)[0]
-    console.log(randLocation)
 
     gBoard[randLocation.i][randLocation.j] = createMine()
     gMines.push(gBoard[randLocation.i][randLocation.j])
@@ -280,3 +285,70 @@ function updateNegs() {
     }
   }
 }
+
+function changeLevel(level) {
+  var gameLevel = {}
+
+  switch (level) {
+    case 0:
+      gameLevel = { size: 4, mines: 2 }
+      break
+    case 1:
+      gameLevel = { size: 8, mines: 18 }
+      break
+    case 2:
+      gameLevel = { size: 12, mines: 32 }
+      break
+    default:
+      gameLevel = { size: 4, mines: 2 }
+      break
+  }
+
+  restart(gameLevel)
+}
+
+function showNegs1(board, cellI, cellJ) {
+  //handle current cell - model and DOM
+  gBoard[cellI][cellJ].isShown = true
+  gGame.shownCount++
+  console.log('gGame.shownCount: ', gGame.shownCount)
+  var elCell = document.querySelector(`.cell-${cellI}-${cellJ}`)
+  elCell.classList.add('shown')
+  renderCell({ i: cellI, j: cellJ }, '')
+  //check i and j growing only (direction right-bottom)
+  for (let i = cellI; i <= cellI + 1; i++) {
+    if (i < 0 || i >= board.length) continue
+
+    for (let j = cellJ + 1; j <= cellJ + 1; j++) {
+      if (j < 0 || j >= board[0].length) continue
+      if (i === cellI && j === cellJ) continue
+
+      var currCell = board[i][j]
+      if (currCell.isShown || currCell.isMarked) continue
+      if (currCell.isMine) continue
+      if (!currCell.mineNegsCount && !currCell.isMine) {
+        // update the model:
+        // board[i][j].isShown - true
+        // gGame.shownCount++
+        // console.log(gGame.shownCount)
+        // update DOM
+        // var elCell = document.querySelector(`.cell-${i}-${j}`)
+        // elCell.classList.add('shown')
+        // renderCell({ i: i, j: j }, '')
+        showNegs1(board, i, j)
+      }
+      if (currCell.mineNegsCount) {
+        // update model
+        board[i][j].isShown = true
+        gGame.shownCount++
+        console.log(gGame.shownCount)
+        // update DOM
+        var elCell = document.querySelector(`.cell-${i}-${j}`)
+        elCell.classList.add('shown')
+        renderCell({ i: i, j: j }, board[i][j].mineNegsCount)
+      }
+    }
+  }
+}
+//TODO
+// Negs1 on the opposite direction (top-left)
